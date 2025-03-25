bd29056437f27b27fd80f4ffb5a3431f9fc3f45c
unknown <Alan-JM>
2025-03-25 13:04:53 -0600

    Primera version

---
+++ src/main/java/org/gerdoc/vista/Menu.java
@@ -0,0 +1,11 @@
+package org.gerdoc.vista;
+
+public class Menu {
+    public static void Estado(){
+        System.out.println("Ingrese Estado");
+    }
+    public static void Menu1(){
+        System.out.println("Estado");
+    }
+
+}
